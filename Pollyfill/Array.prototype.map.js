a44efb40a54d42027f65b0d360c98c387ec8c024
Ashish Katiyar <katiyarashish24@gmail.com>
2026-08-20 21:56:44 +0530

    added edge case for Map pollyfill method

--- Pollyfill/Array.prototype.map.js
+++ Pollyfill/Array.prototype.map.js
@@ -8,3 +8,18 @@ Array.prototype.myMap = function (cbFunction) {
   }
   return result;
 };
+
+// NOTE - What's missing (the follow-up gotchas an interviewer will hit you with)
+// No thisArg support — native map accepts a second argument to set this inside the callback
+// NOTE - What is thisArg  - A value to use as this when executing callbackFn
+
+// arr.map(function(x) { return x * this.multiplier }, { multiplier: 2 })
+
+Array.prototype.myMap = function (cbFunction, thisArg) {
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    // cbFunction(element, index, array)
+    result.push(cbFunction.call(thisArg, this[i], i, this));
+  }
+  return result;
+};
