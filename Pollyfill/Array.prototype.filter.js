5a50f5c96d30561e7d5d4bb463162d4d301c62ea
Ashish Katiyar <katiyarashish24@gmail.com>
2026-08-21 22:55:55 +0530

    updated the filter pollyfill with thisArg

--- Pollyfill/Array.prototype.filter.js
+++ Pollyfill/Array.prototype.filter.js
@@ -9,3 +9,14 @@ Array.prototype.myFilter = function (cbFunc) {
   }
   return result;
 };
+
+// Solution 2
+Array.prototype.myFilter = function (cbFunc, thisArg) {
+  let result = [];
+  for (let i = 0; i < this.length; i++) {
+    if (cbFunc.call(thisArg, this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  return result;
+};
